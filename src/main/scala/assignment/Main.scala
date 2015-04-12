3c9a986bc83979bceb7fbdbde1f6586c5880076c
Thangiee <Thangiee0@gmail.com>
2015-04-12 16:23:40 -0500

    working solution

--- src/main/scala/assignment/Main.scala
+++ src/main/scala/assignment/Main.scala
@@ -1,26 +1,117 @@
 package assignment
 
-import java.awt.{Color, BorderLayout, Dimension}
-import javax.media.opengl.{GLCapabilities, GLProfile}
-import javax.swing.{JFrame, JPanel}
+import java.awt.event.{AWTEventListener, KeyEvent}
+import java.awt.{AWTEvent, BorderLayout, Dimension}
+import javax.swing.{JFileChooser, JFrame, JPanel}
 
-object Main extends App {
+import assignment.Projection.{Parallel, Perspective}
+import utils._
+
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
+object Main extends App with AWTEventListener {
+  private val vertexes      = new ListBuffer[Vertex]()
+  private val faces         = new ListBuffer[Face]()
+  private val cameras       = new ListBuffer[Camera]()
+  private var inputFile     = "patches_06.txt"
+  private var resolution    = 4
+  private val ctrlPoints = ArrayBuffer[Double]()
+
+  private val frame = new JFrame()
+  private var canvas = new MyCanvas(vertexes, faces, cameras, ctrlPoints, resolution)
+  private val panel  = new JPanel(new BorderLayout())
+
   init()
 
-  private def init(): Unit = {
-    val frame = new JFrame()
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  def init(): Unit = {
+    // parse cameras data
+    io.Source.fromFile("cameras_06.txt").getLines().mkString("\n").split("\n").foreach { line =>
+      line.head match {
+        case 'c' => cameras += Camera()
+        case 'i' => cameras.last.name = line.split(" ").last
+        case 't' => cameras.last.projType = if (line.contains("parallel")) Parallel else Perspective
+        case 'e' => cameras.last.vrp = parseVRP(line)
+        case 'l' => cameras.last.vpn = parseVPN(line)
+        case 'u' => cameras.last.vup = parseVUP(line)
+        case 'w' => cameras.last.viewVolume = parseViewVolume(line)
+        case 's' => cameras.last.viewport = parseViewport(line)
+      }
+    }
 
-    val glp      = GLProfile.getDefault
-    val caps     = new GLCapabilities(glp)
-
-    val canvas = new MyCanvas(caps)
-    val panel = new JPanel(new BorderLayout())
     panel.add(canvas, BorderLayout.CENTER)
 
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.getToolkit.addAWTEventListener(this, AWTEvent.KEY_EVENT_MASK)
     frame.add(panel)
     frame.setPreferredSize(new Dimension(1280, 768))
     frame.pack()
     frame.setVisible(true)
+  }
+
+  override def eventDispatched(event: AWTEvent): Unit = {
+    event match {
+      case e: KeyEvent => e.getID match {
+        case KeyEvent.KEY_TYPED => keyTyped(e)
+        case KeyEvent.KEY_PRESSED => keyPressed(e)
+        case _ => e.consume()
+      }
+    }
+  }
+
+  private def keyTyped(e: KeyEvent): Unit = {
+    e.getKeyChar match {
+      case 'n' => promptFilename()
+      case 'd' => loadAndDisplay()
+      case 'x' => canvas.rotate(5, 'x')
+      case 'X' => canvas.rotate(-5, 'x')
+      case 'y' => canvas.rotate(5, 'y')
+      case 'Y' => canvas.rotate(-5, 'y')
+      case 'z' => canvas.rotate(5, 'z')
+      case 'Z' => canvas.rotate(-5, 'z')
+      case 's' => canvas.scale(.05)
+      case 'S' => canvas.scale(-.05)
+      case 'f' => canvas.moveEye('f')
+      case 'b' => canvas.moveEye('b')
+      case 'r' => canvas.changeResolution(-1)
+      case 'R' => canvas.changeResolution(1)
+      case 'p' =>
+      case _ =>
+    }
+  }
+
+  private def keyPressed(e: KeyEvent): Unit = {
+    e.getKeyCode match {
+      case KeyEvent.VK_LEFT => canvas.moveEye('l')
+      case KeyEvent.VK_RIGHT => canvas.moveEye('r')
+      case KeyEvent.VK_UP => canvas.moveEye('u')
+      case KeyEvent.VK_DOWN => canvas.moveEye('d')
+      case _ =>
+    }
+  }
+
+  private def promptFilename(): Unit = {
+    val fc = new JFileChooser("./")
+    if (fc.showOpenDialog(panel) == 0) inputFile = fc.getSelectedFile.getAbsolutePath
+  }
+
+  private def loadAndDisplay(): Unit = {
+    println(s"load $inputFile")
+
+    Seq(vertexes, faces, ctrlPoints).foreach(_.clear())
+    io.Source.fromFile(inputFile).getLines().mkString("\n").split("\n").foreach { line =>
+      line.head match {
+        case 'n' => resolution = parseInt(line)
+        case 'b' => ctrlPoints ++= parseControlPoint(line)
+        case 'v' => vertexes += parseVertex(line)
+        case 'f' => faces += parseFace(line)
+        case _   =>
+      }
+    }
+
+    panel.remove(canvas)
+    canvas = new MyCanvas(vertexes, faces, cameras, ctrlPoints, resolution)
+    panel.add(canvas)
+    frame.pack()
+    panel.requestFocus()
   }
 }
--- src/main/scala/assignment/MyCanvas.scala
+++ src/main/scala/assignment/MyCanvas.scala
@@ -18,13 +18,8 @@ import utils._
 import scala.collection.JavaConversions._
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
-class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener with KeyListener {
-  private val vertexes      = new ListBuffer[Vertex]()
-  private val faces         = new ListBuffer[Face]()
-  private val cameras       = new ListBuffer[Camera]()
-  private val textRenderer  = new TextRenderer(new Font("Verdana", Font.BOLD, 12))
-  private var inputFile     = "patches_06.txt"
-  private var resolution    = 4
+class MyCanvas(vertexes: Seq[Vertex], faces: Seq[Face], cameras: Seq[Camera], ctrlPts: Seq[Double], var resolution: Int) extends GLCanvas() with GLEventListener {
+  private val textRenderer = new TextRenderer(new Font("Verdana", Font.BOLD, 12))
 
   private var glu: GLU = _
   private var gl : GL2 = _
@@ -37,45 +32,10 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
   private var transZ = 0.0
   private var scale  = 1.0
 
-  setSize(640, 480)
   setLocation(100, 100)
   addGLEventListener(this)
-  addKeyListener(this)
-
-  val ctrlPoints = ArrayBuffer[Double](
-    -1.5, -1.5, 4.0,
-    -0.5, -1.5, 2.0,
-    0.5, -1.5, -1.0,
-    1.5, -1.5, 2.0,
-    -1.5, -0.5, 1.0,
-    -0.5, -0.5, 3.0,
-    0.5, -0.5, 0.0,
-    1.5, -0.5, -1.0,
-    -1.5, 0.5, 4.0,
-    -0.5, 0.5, 0.0,
-    0.5, 0.5, 3.0,
-    1.5, 0.5, 4.0,
-    -1.5, 1.5, -2.0,
-    -0.5, 1.5, -2.0,
-    0.5, 1.5, 0.0,
-    1.5, 1.5, -1.0
-  )
 
   override def init(glAutoDrawable: GLAutoDrawable): Unit = {
-    // parse cameras data
-    io.Source.fromFile("cameras_06.txt").getLines().mkString("\n").split("\n").foreach { line =>
-      line.head match {
-        case 'c' => cameras += Camera()
-        case 'i' => cameras.last.name = line.split(" ").last
-        case 't' => cameras.last.projType = if (line.contains("parallel")) Parallel else Perspective
-        case 'e' => cameras.last.vrp = parseVRP(line)
-        case 'l' => cameras.last.vpn = parseVPN(line)
-        case 'u' => cameras.last.vup = parseVUP(line)
-        case 'w' => cameras.last.viewVolume = parseViewVolume(line)
-        case 's' => cameras.last.viewport = parseViewport(line)
-      }
-    }
-
     glu = new GLU()
     gl = glAutoDrawable.getGL.getGL2
 
@@ -84,7 +44,7 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
     gl.glEnable(GL.GL_SCISSOR_TEST)
     gl.glDepthFunc(GL.GL_LEQUAL)
     gl.glEnable(GL2.GL_MAP2_VERTEX_3)
-    gl.glMap2d(GL2.GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, DoubleBuffer.wrap(ctrlPoints.toArray))
+    println(ctrlPts.size)
 
     val animator = new FPSAnimator(this, 30)
     animator.start()
@@ -151,7 +111,9 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
   private def render(): Unit = {
     gl.glColor3f(1, 0, 0)
 
-    if (true) {
+    ctrlPts.grouped(48).toList.foreach { patch =>
+
+      gl.glMap2d(GL2.GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, DoubleBuffer.wrap(patch.toArray))
       // draw patches
       for ( j ← 0 to resolution ) {
         gl.glBegin(GL.GL_LINE_STRIP)
@@ -165,6 +127,7 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
           gl.glEvalCoord2f(j / resolution.toFloat, i / resolution.toFloat)
         }
         gl.glEnd()
+
 
         gl.glBegin(GL.GL_LINE_STRIP)
         for ( i ← 0 to (resolution - j) ) {
@@ -192,62 +155,10 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
       gl.glVertex3d(v3.x, v3.y, v3.z)
       gl.glEnd()
     }
+
   }
 
-  override def keyTyped(e: KeyEvent): Unit = {
-    e.getKeyChar match {
-      case 'n' => promptFilename()
-      case 'd' => loadAndDisplay()
-      case 'x' => rotate(5, 'x')
-      case 'X' => rotate(-5, 'x')
-      case 'y' => rotate(5, 'y')
-      case 'Y' => rotate(-5, 'y')
-      case 'z' => rotate(5, 'z')
-      case 'Z' => rotate(-5, 'z')
-      case 's' => scale(.05)
-      case 'S' => scale(-.05)
-      case 'f' => moveEye('f')
-      case 'b' => moveEye('b')
-      case 'r' => changeResolution(-1)
-      case 'R' => changeResolution(1)
-      case 'p' =>
-      case _   =>
-    }
-  }
-
-  override def keyPressed(e: KeyEvent): Unit = {
-    e.getKeyCode match {
-      case KeyEvent.VK_LEFT  => moveEye('l')
-      case KeyEvent.VK_RIGHT => moveEye('r')
-      case KeyEvent.VK_UP    => moveEye('u')
-      case KeyEvent.VK_DOWN  => moveEye('d')
-      case _                 =>
-    }
-  }
-
-  override def keyReleased(e: KeyEvent): Unit = {}
-
-  private def promptFilename(): Unit = {
-    val fc = new JFileChooser("./")
-    if (fc.showOpenDialog(this) == 0) inputFile = fc.getSelectedFile.getAbsolutePath
-  }
-
-  private def loadAndDisplay(): Unit = {
-    println(s"load $inputFile")
-    reset()
-    io.Source.fromFile(inputFile).getLines().mkString("\n").split("\n").foreach { line =>
-      line.head match {
-        case 'n' => resolution = parseInt(line)
-        case 'b' => ctrlPoints ++= parseControlPoint(line)
-        case 'v' => vertexes += parseVertex(line)
-        case 'f' => faces += parseFace(line)
-        case _   =>
-      }
-    }
-    println(ctrlPoints)
-  }
-
-  private def rotate(degrees: Double, axis: Char): Unit = {
+  def rotate(degrees: Double, axis: Char): Unit = {
     axis match {
       case 'x' | 'X' => angleX += degrees
       case 'y' | 'Y' => angleY += degrees
@@ -255,11 +166,11 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
     }
   }
 
-  private def scale(factor: Double): Unit = {
+  def scale(factor: Double): Unit = {
     scale += factor
   }
 
-  private def moveEye(direction: Char): Unit = {
+  def moveEye(direction: Char): Unit = {
     direction match {
       case 'l' => transX -= .05
       case 'r' => transX += .05
@@ -270,22 +181,11 @@ class MyCanvas(cap: GLCapabilities) extends GLCanvas(cap) with GLEventListener w
     }
   }
 
-  private def changeResolution(delta: Int) = {
+  def changeResolution(delta: Int) = {
     if (resolution > 1 && delta < 0) {
       resolution += delta
     } else if (resolution < 100 && delta > 0) {
       resolution += delta
     }
-  }
-
-  private def reset(): Unit = {
-    Seq(vertexes, faces, ctrlPoints).foreach(_.clear())
-    angleX = 0.0
-    angleY = 0.0
-    angleZ = 0.0
-    transX = 0.0
-    transY = 0.0
-    transZ = 0.0
-    scale = 1.0
   }
 }
